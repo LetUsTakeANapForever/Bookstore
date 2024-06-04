bdccf58ed024ab1649e0909965539fc7d862f93d
Nidchamon <167843858+LetUsTakeANapForever@users.noreply.github.com>
2024-06-04 22:09:32 +0700

    Update BookStore.py
    -Adjust function for GUI windows

--- BookStore.py
+++ BookStore.py
@@ -125,5 +125,122 @@ class Main:
         except:
             return 'Error'
 
-class Cart:
-    pass
+class ManageCustomer:
+    def __init__(self, user):
+        self.current_user = user
+        self.ref = db.reference('')
+        self.ref_books = db.reference('books')
+        self.ref_users = db.reference('users')
+        self.cart_ref = self.ref_users.child(self.current_user+'/cart')
+        self.history_ref = self.ref_users.child(self.current_user+'/order_history')
+        self.total = 0
+
+    def set_total(self, price, qty):
+        self.total = self.total + float(price)*qty
+    
+    def get_total(self):
+        return self.total
+    
+    def get_category(self):
+        try:
+            self.categories = list(self.ref_books.get())
+            return self.categories
+        except Exception as e: print(e) 
+
+    def browse_title(self, category, title):
+        try:
+            ISBN_ref = self.ref_books.child(category+'/' +title+'/ISBN')
+            author_ref = self.ref_books.child(category+'/'   +title+'/author')
+            price_ref = self.ref_books.child(category+'/'    +title+'/price')
+            result = f'Title  : {title}\nISBN   : {ISBN_ref.get()}\nAuthor : {author_ref.get()}\nPrice  : {price_ref.get()}'
+            if (ISBN_ref.get() or author_ref.get() or price_ref.get()) is None:
+                return ' Error'
+            return result
+        except: return 'Error'
+
+    def browse_category(self, category):
+            try:
+                books = list(self.ref_books.child(category).get())
+                return books
+            except: return 'Error'
+
+    def buy_menu(self, category, title):
+        try:
+            price_ref = self.ref_books.child(category+'/'+title+'/price')
+            if price_ref.get() is None:
+                return 'Error'
+            self.add_to_cart(title, price_ref.get())
+            return 'Successfully added'
+        except: print('Error')
+
+    def show_total(self):
+        try:
+            in_cart = list(self.cart_ref.get())
+            for order in in_cart:
+                if not order is None:    
+                    price_ref = self.ref_users.child(self.current_user+'/cart/'+order+'/price')
+                    qty_ref = self.ref_users.child(self.current_user+'/cart/'+order+'/quantity')
+                    self.add_to_order_history(order, price_ref.get())
+                    self.set_total(price_ref.get(), qty_ref.get())
+            self.cart_ref.delete()
+            return '%.2f' %self.get_total()
+        except: return 'Error'
+    
+    def show_cart(self):
+        cart_list = []
+        try:
+            in_cart = list(self.cart_ref.get())
+            for order in in_cart:
+                if not order is None:    
+                    price_ref = self.ref_users.child(self.current_user+'/cart/'+order+'/price')
+                    qty_ref = self.ref_users.child(self.current_user+'/cart/'+order+'/quantity')
+                    item = [(f'{order} {price_ref.get()} THB (qty:{qty_ref.get()})')]
+                    cart_list.append(item)
+            return cart_list
+        except: print('Error')
+
+    def add_to_cart(self, title, price):
+        try:
+            qty = 1
+            if not self.cart_ref.get() is None:
+                if title in list(self.cart_ref.get()):
+                    qty_ref = self.ref_users.child(self.current_user+'/cart/'+title+'/quantity')
+                    qty = qty_ref.get() + 1
+            self.cart_ref.update({
+                title : {
+                    'price' : price,
+                    'quantity' : qty
+                }      
+            })
+        except: print('Error')
+
+    def add_to_order_history(self, title, price):
+        try:
+            cart_qty_ref = self.ref_users.child(self.current_user+'/cart/'+title+'/quantity')
+            qty = cart_qty_ref.get()
+            if not self.history_ref.get() is None:
+                if title in list(self.history_ref.get()):
+                    qty_ref = self.ref_users.child(self.current_user+'/order_history/'+title+'/quantity')
+                    qty = qty_ref.get() + cart_qty_ref.get()        
+            self.history_ref.update({
+                title : {
+                    'price' : price,
+                    'quantity' : qty
+                }  
+            })
+        except: print('Error')
+
+    def show_order_history(self):
+        order_hist = []
+        try:
+            if self.history_ref.get() is None:
+                order_hist = [['Empty']]
+            else:
+                orders_hist = list(self.history_ref.get())
+                for order in orders_hist:
+                    price_ref = self.ref_users.child(f'{self.current_user}/order_history/{order}/price')
+                    qty_ref = self.ref_users.child(f'{self.current_user}/order_history/{order}/quantity')
+                    hist_list = [f'- {order} {price_ref.get()} THB (qty:{qty_ref.get()})']
+                    order_hist.append(hist_list)
+            return order_hist
+        except: return 'Error'
